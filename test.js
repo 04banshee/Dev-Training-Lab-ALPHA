4d9ed9b1027844bdaa2f2a81da06b8c995d6b0f1
04banshee <dwiles@una.edu>
2021-08-26 16:58:38 -0500

    New file stucture and files added

--- test.js
+++ test.js
@@ -1,12 +1,17 @@
+const fs = require('fs');
+const express = require('express');
+const app = express();
+const http = require('http');
 
-console.log('hello node.  How are we today?')
-const express = require('express')
-const app = express()
-const port = 3000
 
-app.get('/', (req, res) => {
- res.send('hello node!!  This is a new app.')})
- 
-app.listen(port, () => {
-  console.log(`Node Express is listening on port 3000`)
-})
+const server = http.createServer(app);
+server.listen(8080, function() {
+  console.log((new Date()) + ' Server is listening on port 8080');
+});
+
+app.use(express.static(__dirname));
+app.get('/', function(req, res) {
+console.log('app.get slash');
+const file = fs.readFileSync('index1.html', {encoding: 'utf8'});
+res.send(file);
+});
