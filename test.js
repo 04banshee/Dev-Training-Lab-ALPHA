e4e62168085f66f8bfefa2aed24cc8160d8aaea7
04banshee <dwiles@una.edu>
2021-08-29 10:47:03 -0500

    Update to test file

--- test.js
+++ test.js
@@ -1,10 +1,10 @@
 
-var fs = require('fs');
-var express = require('express');
-var app = express();
-var http = require('http');
+const fs = require('fs');
+const express = require('express');
+const app = express();
+const http = require('http');
 
-var server = http.createServer(app);
+const server = http.createServer(app);
 server.listen(3000, function() {
   console.log((new Date()) + ' Server is listening on port 3000');
 });
@@ -12,6 +12,6 @@ server.listen(3000, function() {
 app.use(express.static(__dirname));
 app.get('/', function(req, res) {
  console.log('app.get slash');
- var file = fs.readFileSync('index1.html', {encoding: 'utf8'});
+ const file = fs.readFileSync('index1.html', {encoding: 'utf8'});
  res.send(file);
 });
